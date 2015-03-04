1fc8554ee888e1d80e90046291110919aeae2905
Brandon Dang <frogninja@sbcglobal.net>
2015-03-03 22:33:06 -0600

    added player sprite
    added player sprite, changed proportions and set anchor to center of
    body

--- build/web/js/bomber.js
+++ build/web/js/bomber.js
@@ -1,15 +1,16 @@
 var game = new Phaser.Game(672, 480, Phaser.AUTO, 'game');
 
 var PhaserGame = function(game) {
-    
+    this.player = null;
 }
 
 PhaserGame.prototype = {
     
     init: function() {
-        
+        this.physics.startSystem(Phaser.Physics.ARCADE);
     },
     
+    // note: graphics copyright 2015 Photon Storm Ltd
     preload: function() {    
         this.load.tilemap('map', 'assets/map.json', null, Phaser.Tilemap.TILED_JSON);
         this.load.image('tiles', 'assets/tiles.png');
@@ -17,12 +18,16 @@ PhaserGame.prototype = {
         this.load.image('bomb', 'assets/bomb.png');
         this.load.spritesheet('dude', 'assets/dude.png', 32, 48);
         this.load.spritesheet('baddie', 'assets/baddie.png', 32, 48);*/
+        this.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     },
             
     create: function() {
         this.map = this.add.tilemap('map');
         this.map.addTilesetImage('tiles', 'tiles');
         this.layer = this.map.createLayer('Tile Layer 1');
+        player = game.add.sprite(48, 48, 'dude', 4);
+        player.anchor.set(0.5);
+        player.scale.set(.8, .58);
     }
         
 };
