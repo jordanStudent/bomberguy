6e2e421ff501226ce883953c83b2c50f962b9677
Brandon Dang <frogninja@sbcglobal.net>
2015-03-06 14:15:59 -0600

    title update, collision/movement updates
    changed index title, updated basic collision/movement of sprite

--- web/js/bomber.js
+++ web/js/bomber.js
@@ -1,36 +1,54 @@
-var game = new Phaser.Game(672, 480, Phaser.AUTO, 'game');
+var game = new Phaser.Game(672, 480, Phaser.AUTO, 'game', {preload: preload, create: create, update: update, move: move});
 
-var PhaserGame = function(game) {
-    this.player = null;
+var map = null;
+var layer = null;
+var player = null;
+var speed = 150;
+var current = Phaser.UP;
+
+// note: graphics copyright 2015 Photon Storm Ltd
+function preload() {    
+    game.load.tilemap('map', 'assets/map.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('tiles', 'assets/tiles.png');
+    /*this.load.image('firstaid', 'assets/firstaid.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    this.load.spritesheet('baddie', 'assets/baddie.png', 32, 48);*/
+    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 
-PhaserGame.prototype = {
+function create() {
+    map = game.add.tilemap('map');
+    map.addTilesetImage('tiles', 'tiles');
+    layer = map.createLayer('Tile Layer 1');
     
-    init: function() {
-        this.physics.startSystem(Phaser.Physics.ARCADE);
-    },
+    map.setCollision(6, true, this.layer);
     
-    // note: graphics copyright 2015 Photon Storm Ltd
-    preload: function() {    
-        this.load.tilemap('map', 'assets/map.json', null, Phaser.Tilemap.TILED_JSON);
-        this.load.image('tiles', 'assets/tiles.png');
-        /*this.load.image('firstaid', 'assets/firstaid.png');
-        this.load.image('bomb', 'assets/bomb.png');
-        this.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-        this.load.spritesheet('baddie', 'assets/baddie.png', 32, 48);*/
-        this.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-    },
-            
-    create: function() {
-        this.map = this.add.tilemap('map');
-        this.map.addTilesetImage('tiles', 'tiles');
-        this.layer = this.map.createLayer('Tile Layer 1');
-        player = game.add.sprite(48, 48, 'dude', 4);
-        player.anchor.set(0.5);
-        player.scale.set(.8, .58);
+    player = game.add.sprite(48, 48, 'dude', 4);
+    player.anchor.set(0.5);
+    player.scale.set(.8, .58);
+    
+    game.physics.arcade.enable(player);
+    move(Phaser.DOWN);
+}
+  
+function update() {
+    game.physics.arcade.collide(player, layer);
+}
+
+function move(direction) {
+    var velocity = speed;
+    
+    if(direction === Phaser.LEFT || direction === Phaser.UP) {
+        velocity = -velocity;
     }
-        
-};
+    if(direction === Phaser.LEFT || direction === Phaser.RIGHT) {
+        player.body.velocity.x = velocity;
+    } else {
+        player.body.velocity.y = velocity;
+    }
+    
+    current = direction;
+}
 
-
-game.state.add('Game', PhaserGame, true);
+//game.state.add('Game', PhaserGame, true);
